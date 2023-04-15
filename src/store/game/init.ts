666a78011e38def334a2a92809686dbeafae4d79
Omhet <omhetehmo@yandex.ru>
2023-04-15 19:17:07 +0200

    remove unused

--- src/store/game/init.ts
+++ src/store/game/init.ts
@@ -2,10 +2,8 @@ import { GameStatus } from '@app-types/game'
 import { levelDataManager } from '@lib/levels/LevelDataManager'
 import { setCurrentLevelNumber } from '@store/levels'
 import { openGameEndModal, openGameStartModal } from '@store/modals'
-import { createEvent, guard } from 'effector'
 import {
     endGame,
-    gameStatusStore,
     gameStore,
     gameStoreInitial,
     loadGame,
@@ -22,13 +20,9 @@ gameStore
 
 loadGame.use(async (levelNumber: number) => {
     setIsLoading(true)
-
     resetGameData()
-
     setCurrentLevelNumber(levelNumber)
-
     setIsLoading(false)
-
     openGameStartModal()
 })
 
@@ -37,38 +31,7 @@ startGame.watch(() => {
     levelDataManager.playLevelMusic()
 })
 
-const preGameEnd = createEvent()
-// Set end game status
-// guard({
-//     source: gameStore,
-//     filter: ({ status, blastCount, noteCount }) =>
-//         status === GameStatus.InProgress && (blastCount === 0 || noteCount === 0),
-//     target: preGameEnd,
-// })
-preGameEnd.watch(() => {
-    setTimeout(() => {
-        setGameStatus(GameStatus.End)
-    }, 300)
-})
-// Call end game
-guard({
-    source: gameStatusStore,
-    filter: (status) => status === GameStatus.End,
-    target: endGame,
-})
-
 // Do end game stuff
 endGame.watch(() => {
-    // const { score: oldScore } = currentLevelStore.getState()
-
-    // const newScore = hitCount
-    // setCurrentLevelScore(newScore)
-    // if (newScore > oldScore) {
-    //     setIsBetterScoreThanEarlier(true)
-    //     rewriteCurrentLevelScore(newScore)
-    // } else {
-    //     setIsBetterScoreThanEarlier(false)
-    // }
-
     openGameEndModal()
 })
